49b0393bf035949616d50b111ad10a2224652b58
roshinkv007 <roshinkv007@gmail.com>
2025-12-03 13:10:48 +0530

    change done

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const heading = React.createElement("div", { id: "parent" }, [
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", null, "Hello world from react"),
-    React.createElement("h2", {}, "This is h2 tag"),
+    React.createElement("h2", {}, "This is h2 tag 2"),
   ]),
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
