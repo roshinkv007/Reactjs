ca452a2988d52db6acdccf979963cb67aa96d656
roshinkv007 <roshinkv007@gmail.com>
2025-12-04 12:59:49 +0530

    day2 of react

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-{
+import React from "react";
+import ReactDOM from "react-dom/client";
+/* {
   /* <div id="parent">
   <div id="child">
     <h1>Hello world from react</h1>
@@ -8,17 +10,17 @@
     <h1>Hello world from react</h1>
     <h2>This is h2 tag</h2>
   </div>
-</div>; */
-}
+</div>; 
+} */
 
 const heading = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", null, "Hello world from react"),
-    React.createElement("h2", {}, "This is h2 tag"),
+  React.createElement("div", { id: "child", key:"1"}, [
+    React.createElement("h1",{key:"a"}, "Hello world from react"),
+    React.createElement("h2", {key:"b"}, "This is h2 tag"),
   ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", null, "Hello world from react"),
-    React.createElement("h2", {}, "This is h2 tag 2"),
+  React.createElement("div", { id: "child2" ,key:"2" }, [
+    React.createElement("h1",{key:"c"}, "Hello world from react-day2"),
+    React.createElement("h2", {key:"d"}, "This is h2 tag 2"),
   ]),
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
---
+++ dist/React.6bd02f5a.js
@@ -0,0 +1,31 @@
+/* {
+  /* <div id="parent">
+  <div id="child">
+    <h1>Hello world from react</h1>
+    <h2>This is h2 tag</h2>
+  </div>
+  <div id="child2">
+    <h1>Hello world from react</h1>
+    <h2>This is h2 tag</h2>
+  </div>
+</div>; 
+} */ const heading = React.createElement("div", {
+    id: "parent"
+}, [
+    React.createElement("div", {
+        id: "child"
+    }, [
+        React.createElement("h1", null, "Hello world from react"),
+        React.createElement("h2", {}, "This is h2 tag")
+    ]),
+    React.createElement("div", {
+        id: "child2"
+    }, [
+        React.createElement("h1", null, "Hello world from react"),
+        React.createElement("h2", {}, "This is h2 tag 2")
+    ])
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=React.6bd02f5a.js.map
