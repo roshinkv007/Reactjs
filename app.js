c57fdd88e65e3f40e311f11a9af03ca683610356
roshinkv007 <roshinkv007@gmail.com>
2025-12-05 12:44:15 +0530

    deleted nodemodules,parcelcache,dist

--- app.js
+++ app.js
@@ -12,8 +12,29 @@ import ReactDOM from "react-dom/client";
   </div>
 </div>; 
 } */
+const Hello=()=>{
+ return "This is a Component"
+}
 
-const heading = React.createElement("div", { id: "parent" }, [
+const heading02=(
+  <div>
+    <Hello/>
+  </div>
+)
+console.log(heading02);
+
+
+const heading=(
+  <div>
+    <h1>This is day3 react using jsx</h1>
+     <div>
+  </div>
+  </div>
+ 
+)
+console.log(heading)
+
+/* const heading = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child", key:"1"}, [
     React.createElement("h1",{key:"a"}, "Hello world from react"),
     React.createElement("h2", {key:"b"}, "This is h2 tag"),
@@ -22,6 +43,9 @@ const heading = React.createElement("div", { id: "parent" }, [
     React.createElement("h1",{key:"c"}, "Hello world from react-day2"),
     React.createElement("h2", {key:"d"}, "This is h2 tag 2"),
   ]),
-]);
+]); */
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
+
+const root2 = ReactDOM.createRoot(document.getElementById("root2"));
+root2.render(heading02);
