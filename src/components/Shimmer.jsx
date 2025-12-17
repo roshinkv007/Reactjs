3e4248ec9c82eca81ab298cf51b6cbe8b1c04fd2
roshinkv007 <roshinkv007@gmail.com>
2025-12-18 01:19:52 +0530

    new shimmer added

--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -1,4 +1,4 @@
-const Shimmer = () => {
+/* const Shimmer = () => {
   return (
     <div className="shimmer-container">
       <div className="shimmer-card"></div>
@@ -16,6 +16,27 @@ const Shimmer = () => {
       <div className="shimmer-card"></div>
       <div className="shimmer-card"></div>
       <div className="shimmer-card"></div>
+    </div>
+  );
+};
+
+export default Shimmer;
+ */
+const Shimmer = () => {
+  return (
+    <div className="restau-cards">
+      {Array(8)
+        .fill("")
+        .map((_, index) => (
+          <div className="shimmer-card" key={index}>
+            <div className="shimmer-img"></div>
+            <div className="shimmer-content">
+              <div className="shimmer-line title"></div>
+              <div className="shimmer-line subtitle"></div>
+              <div className="shimmer-line small"></div>
+            </div>
+          </div>
+        ))}
     </div>
   );
 };
